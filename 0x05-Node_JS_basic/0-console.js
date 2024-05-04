c301a08127ca791cc1c03af9de48d85b635b1f8f
RedolF <redolf639@gmail.com>
2024-05-04 18:06:10 +0000

    node basics

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Function displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Samuel Koomson
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
